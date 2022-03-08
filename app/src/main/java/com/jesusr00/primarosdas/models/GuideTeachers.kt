4d83a8ce1f9bdd7fb93a5aeab6efb76484e191d6
Jesus R Lopez Martin <jesus.reikel@gmail.com>
2022-03-08 12:58:09 -0500

    refactor database

--- app/src/main/java/com/jesusr00/primarosdas/models/GuideTeachers.kt
+++ app/src/main/java/com/jesusr00/primarosdas/models/GuideTeachers.kt
@@ -1,6 +1,6 @@
 package com.jesusr00.primarosdas.models
 
-class GuideTeachers (val name: String, val lastName: String, val username: String, val photo: ByteArray?, val group: Int,) {
+class GuideTeachers (val name: String, val lastName: String, val username: String, val group: Int, val photo: ByteArray?) {
 
     override fun toString(): String {
         return "GuideTeachers(name='$name', lastName='$lastName', username='$username', group=$group, photo=${photo?.contentToString()})"
