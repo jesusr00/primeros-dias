71b9f002862123f1d6b4fae539d5c34edb850b52
Jesus R Lopez Martin <jesus.reikel@gmail.com>
2022-04-01 13:56:33 -0400

    Copy assets in splash activity

--- app/src/main/java/com/jesusr00/primerosdias/SplashActivity.kt
+++ app/src/main/java/com/jesusr00/primerosdias/SplashActivity.kt
@@ -3,10 +3,14 @@ package com.jesusr00.primerosdias
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
+import com.jesusr00.primerosdias.utils.CustomAssetsManager
 import kotlinx.coroutines.*
+import java.io.File
 
 class SplashActivity : AppCompatActivity() {
+
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,20 @@ class SplashActivity : AppCompatActivity() {
         )
 
         GlobalScope.launch(Dispatchers.Main) {
+            val customAssetsManager = CustomAssetsManager(this@SplashActivity)
+            val images = customAssetsManager.getImages()
+            val map = customAssetsManager.getMap()
+            if (images.isEmpty()) {
+                Log.d("SplashActivity", "No hay imagenes")
+                Log.d("SplashActivity", "Copiando imagenes")
+                customAssetsManager.copyAssetsImages()
+            }
+            if (!map.exists()){
+                Log.d("SplashActivity", "No hay mapa")
+                Log.d("SplashActivity", "Copiando mapa")
+                customAssetsManager.copyAssetsMap()
+            }
+
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             //delay(3000)
             delay(30)
